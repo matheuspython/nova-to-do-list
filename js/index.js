392b38dd507dc4a0f11625f4c8bc4b1db092c7d4
matheus souza <ruivorobado@gmail.com>
2021-01-10 14:11:49 -0300

    refazendo os scritps

--- js/index.js
+++ js/index.js
@@ -1,29 +1,3 @@
-const novaTarefa = document.querySelector('.input-nova-tarefa')
-const btn = document.querySelector('.btn-add-tarefa')
-const listaUl = document.querySelector('.tarefas')
-
-let indice = 0
-
-btn.addEventListener('click', () => {
-    newTarefa = document.createElement('li')
-    newTarefaText = document.createTextNode(novaTarefa.value)
-    newTarefa.appendChild(newTarefaText)
-
-
-    newTarefaButton = document.createElement("button")
-    newTarefaButton.addEventListener('click', apagaElemento(indice))
-
-    newTarefaButtonText = document.createTextNode('apagar')
-    newTarefaButton.appendChild(newTarefaButtonText)
-
-    newTarefa.appendChild(newTarefaButton)
-
-
-    listaUl.appendChild(newTarefa)
-    indice++
-
-})
-
-function apagaElemento(indic) {
-    alert(indic)
-}
+const novaTarefa = document.querySelector('.input-nova-tarefa') //texto
+const btn = document.querySelector('.btn-add-tarefa') //botao pra enviar
+const listaUl = document.querySelector('.tarefas') //lista ul
